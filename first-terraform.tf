e1dbf66c802c1c270488bb7c8b54d0e51ab71e58
kenkool23 <62091494+kenkool23@users.noreply.github.com>
2020-07-22 05:15:37 -0400

    deleting key

--- first-terraform.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA5PH43FLIPNTNUTUH"
-  secret_key = "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"
-}
-resource "aws_instance" "myec2" {
-  ami = "ami-08f3d892de259504d"
-  instance_type = "t2.micro"
-}
